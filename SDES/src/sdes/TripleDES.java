2d72a7522ec18955a7cec66dfeaa28910935ff26
jesus-r-mendoza <jesus_r_mendoza@yahoo.com>
2018-10-24 10:46:52 -0700

    added trial code ; untested

--- SDES/src/sdes/TripleDES.java
+++ SDES/src/sdes/TripleDES.java
@@ -2,12 +2,23 @@ package sdes;
 
 public class TripleDES {
 
-
     public static byte[] Encrypt(byte[] rawkey1, byte[] rawkey2, byte[] plaintext) {
+
+        byte[] inner, outer, last;
+        inner = SDES.Encrypt(rawkey1, plaintext);
+        outer = SDES.Decrypt(rawkey2, inner);
+        last = SDES.Encrypt(rawkey1, outer);
+        return last;
 
     }
 
     public static byte[] Decrypt(byte[] rawkey1, byte[] rawkey2, byte[] ciphertext) {
+
+        byte[] inner, outer, last;
+        inner = SDES.Decrypt(rawkey1, ciphertext);
+        outer = SDES.Encrypt(rawkey2, inner);
+        last = SDES.Decrypt(rawkey1, outer);
+        return last;
 
     }
 
