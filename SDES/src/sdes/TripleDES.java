fd8ac9e2d1d6b0d958b6381a2fc41c9c5f57cd90
jesus-r-mendoza <jesus_r_mendoza@yahoo.com>
2018-10-24 10:34:00 -0700

    added method headers

---
+++ SDES/src/sdes/TripleDES.java
@@ -0,0 +1,14 @@
+package sdes;
+
+public class TripleDES {
+
+
+    public static byte[] Encrypt(byte[] rawkey1, byte[] rawkey2, byte[] plaintext) {
+
+    }
+
+    public static byte[] Decrypt(byte[] rawkey1, byte[] rawkey2, byte[] ciphertext) {
+
+    }
+
+}
