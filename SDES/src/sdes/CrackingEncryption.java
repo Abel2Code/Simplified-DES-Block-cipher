efac95c57bde6cdbe1b38e4ff080ef3c632113f6
jesus-r-mendoza <jesus_r_mendoza@yahoo.com>
2018-11-05 17:51:21 -0800

    finished problem 1 ; works and prints results

--- SDES/src/sdes/CrackingEncryption.java
+++ SDES/src/sdes/CrackingEncryption.java
@@ -7,22 +7,11 @@ import java.util.Scanner;
 public class CrackingEncryption {
     
     public static void main(String[] args) {
-                
-        String word = "CRYPTOGRAPHY";
-        
-        byte[] encry = CASCII.Convert(word);
         
         System.out.println();
-        for ( int i = 0; i < encry.length; i++ ) {
-            if ( i == 32 )
-                System.out.println();
-            else if (i != 0 && i % 4 == 0 ) 
-                System.out.print(" ");
-            System.out.print(encry[i]);
-        }
-        System.out.println("\n");
-        System.out.println(CASCII.toString(encry) + "\n");
-
+        
+        problem1();
+        
         String msg1 = "../msg1.txt";
         String msg2 = "../msg2.txt";
         
@@ -60,6 +49,52 @@ public class CrackingEncryption {
             System.out.println("\nERROR: File Not Found \n");
         }
         return null;
+    }
+    
+    public static void problem1() {
+        byte[] key = {0,1,1,1,0,0,1,1,0,1};
+        String word = "CRYPTOGRAPHY";
+        byte[] plaintext = CASCII.Convert(word);
+        
+        byte[] ciphertext = new byte[plaintext.length];
+        byte[] section = new byte[8];
+        byte[] ciphSect;
+        
+        System.out.println("This is the plain text in CASCII.");
+        System.out.println(word + "\n");
+        
+        System.out.println("This is the byte array of the plain text.");
+        printByteArr(plaintext);
+        
+        for ( int i = 0; i < plaintext.length; i+=0 ) {            
+            for (int x = 0; x < 8; x++, i++) {
+                section[x] = plaintext[i];
+            }
+            ciphSect = SDES.Encrypt(key, section);
+            for (int x = 0, y = i-8; x < 8; x++, y++) {
+                ciphertext[y] = ciphSect[x];
+            }
+        }
+        
+        System.out.println("This is the byte array of the cipher text.");
+        printByteArr(ciphertext);
+        
+        System.out.println("This is the cipher text in CASCII.");
+        System.out.println(CASCII.toString(ciphertext) + "\n");
+
+    }
+    
+    private static void printByteArr(byte[] arr) {
+        System.out.println();
+        int len = arr.length;
+        for ( int i = 0; i < len; i++ ) {
+            if ( i == len/2 )
+                System.out.println();
+            else if (i != 0 && i % 4 == 0 ) 
+                System.out.print(" ");
+            System.out.print(arr[i]);
+        }
+        System.out.println("\n");
     }
     
 }
