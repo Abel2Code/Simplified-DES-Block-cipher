ceb4b68a40a99b2da4e6d80d9e0da1586bcfe0a8
jesus-r-mendoza <jesus_r_mendoza@yahoo.com>
2018-11-05 21:30:35 -0800

    almost have it working ; super close but not yet

--- SDES/src/sdes/CrackingEncryption.java
+++ SDES/src/sdes/CrackingEncryption.java
@@ -9,62 +9,9 @@ public class CrackingEncryption {
     
     public static void main(String[] args) {
         
-        
-        byte[][] keys = permutations(10);
-        
-        for ( byte[] list : keys ) {
-            printByteArr(list);
-        }
-        
-        
-        int count = 0;
-        int n = 10;
-        byte[] key = new byte[(int) Math.pow(2, n)];
-        for ( int i = 0; i < n; i++ ) {
-            for ( int j = i+1; j < n; j++ ) {
-                //System.out.println("i : " + i + "  |  j : " + j);
-                count++;
-            }
-        }
-        System.out.println("\n"+count);
-        
-        //printByteArr(key);
-        
         problem1();
-        //problem2();
+        problem2();        
         
-        
-    }
-    
-    public static byte[] parseFile(String relPath) {
-        
-        try {            
-            File file = new File(relPath);
-                        
-            Scanner reader = new Scanner(file);
-            byte b;
-            String line, sub;         
-            byte[] bytes;
-            
-            // This if statement is assuming that there is only one line, and
-            // that all the bytes are written in that single line.
-            if ( reader.hasNextLine()) {        
-                line = reader.nextLine();
-                bytes = new byte[line.length()];       
-                for ( int i = 0; i < line.length(); i++) {
-                    sub = line.substring(i, i+1);
-                    b = Byte.parseByte(sub);
-                    bytes[i] = b;
-                }
-                return bytes;
-            }
-            reader.close();
-            return null;
-            
-        } catch (FileNotFoundException ex) {
-            System.out.println("\nERROR: File Not Found \n");
-        }
-        return null;
     }
     
     public static void problem1() {
@@ -83,7 +30,7 @@ public class CrackingEncryption {
         System.out.println("This is the plain text in CASCII.");
         System.out.println(word + "\n");
         
-        System.out.println("This is the byte array of the plain text.");
+        System.out.print("This is the byte array of the plain text.");
         printByteArr(plaintext);
         
         for ( int i = 0; i < plaintext.length; i+=0 ) {            
@@ -96,7 +43,7 @@ public class CrackingEncryption {
             }
         }
         
-        System.out.println("This is the byte array of the cipher text.");
+        System.out.print("This is the byte array of the cipher text.");
         printByteArr(ciphertext);
         
         System.out.println("This is the cipher text in CASCII.");
@@ -104,11 +51,15 @@ public class CrackingEncryption {
 
     }
     
-    public static void problem2() {       
+    public static void problem2() {     
+        
+        System.out.println(" - - - - - - Problem 2 - - - - - - - -\n");
+        
         String msg1 = "../msg1.txt";
         byte[] msg1Bytes = parseFile(msg1);
-        String msg2 = "../msg2.txt";
-        byte[] msg2Bytes = parseFile(msg2);
+        
+        //String msg2 = "../msg2.txt";
+        //byte[] msg2Bytes = parseFile(msg2);
         
         //printArrByRows(msg1Bytes, 64);
         //printArrByRows(msg2Bytes, 64);
@@ -116,20 +67,50 @@ public class CrackingEncryption {
         byte[] plainSect;
         byte[] section = new byte[8];
         byte[] ciphertext = new byte[msg1Bytes.length];
+               
+        byte[][] keys = permutations(10);
         
-        
-        for ( int i = 0; i < ciphertext.length; i+=0 ) {            
-            for (int x = 0; x < 8; x++, i++) {
-                section[x] = ciphertext[i];
+        ArrayList<String> deciph = new ArrayList<>();
+                
+        for ( byte[] key : keys ) {
+            for ( int i = 0; i < ciphertext.length; i+=0 ) {            
+                for (int x = 0; x < 8; x++, i++) {
+                    section[x] = msg1Bytes[i];
+                }
+                plainSect = SDES.Decrypt(key, section);
+                for (int x = 0, y = i-8; x < 8; x++, y++) {
+                    ciphertext[y] = plainSect[x];
+                }
             }
-            //plainSect = SDES.Decrypt(key, section);
-            for (int x = 0, y = i-8; x < 8; x++, y++) {
-                //ciphertext[y] = plainSect[x];
-            }
+            
+            deciph.add(CASCII.toString(ciphertext));
+            
+            /*
+            
+            //if (CASCII.toString(ciphertext).indexOf("D") > 0) {
+                String str = CASCII.toString(ciphertext).substring(0,10);                
+                
+                System.out.print(str + " : ");
+                                
+                for ( int x = 0; x < 5; x++ ) {
+                    System.out.print(ciphertext[x]);
+                }
+                System.out.print(" | ");
+                
+                for ( int i = 0; i < str.length(); i++ ) {
+                    System.out.print(CASCII.Convert(str.charAt(i)) + ", ");
+                }
+                System.out.println();
+            //}
+            */
+           
+            
+        } 
+        quickSort(deciph);
+        for ( String s : deciph ) {
+            System.out.println(s + "\n");
         }
-        
-        
-        
+                
     }
     
     private static void printByteArr(byte[] arr) {
@@ -180,5 +161,65 @@ public class CrackingEncryption {
         }
     }
     
+    public static byte[] parseFile(String relPath) {
+        
+        try {            
+            File file = new File(relPath);
+                        
+            Scanner reader = new Scanner(file);
+            byte b;
+            String line, sub;         
+            byte[] bytes;
+            
+            // This if statement is assuming that there is only one line, and
+            // that all the bytes are written in that single line.
+            if ( reader.hasNextLine()) {        
+                line = reader.nextLine();
+                bytes = new byte[line.length()];       
+                for ( int i = 0; i < line.length(); i++) {
+                    sub = line.substring(i, i+1);
+                    b = Byte.parseByte(sub);
+                    bytes[i] = b;
+                }
+                return bytes;
+            }
+            reader.close();
+            return null;
+            
+        } catch (FileNotFoundException ex) {
+            System.out.println("\nERROR: File Not Found \n");
+        }
+        return null;
+    }
     
+    public static <E extends Comparable<E>> void quickSort(ArrayList<E> list) {
+        quickSort(list, 0, list.size()-1);
+    }
+    
+    private static <E extends Comparable<E>> void quickSort(ArrayList<E> list,int low, int high) {
+        if(low < high) {
+            int p = partitian(list, low, high);
+            quickSort(list, low, p-1);
+            quickSort(list, p+1, high);
+        }
+    }
+    
+    private static <E extends Comparable<E>> int partitian(ArrayList<E> list,int low, int high) {
+        E pivot = list.get(high);
+        int i = low - 1;
+        
+        for(int j = low; j < high; j++) {
+            E temp1 = list.get(j);
+            if(temp1.compareTo(pivot) < 0 || temp1.equals(pivot)) {
+                i++;
+                list.set(j, list.get(i));
+                list.set(i, temp1);
+            }
+        }
+        
+        E temp2 = list.get(i+1);
+        list.set(i+1, pivot);
+        list.set(high, temp2);
+        return i+1;
+    }
 }   
