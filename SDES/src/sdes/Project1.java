50d498d22e17d04556ecec1339d1220276f51d7b
jesus-r-mendoza <jesus_r_mendoza@yahoo.com>
2018-11-10 17:32:22 -0800

    file created as main driver ; turning in file that was turned in

---
+++ SDES/src/sdes/Project1.java
@@ -0,0 +1,17 @@
+package sdes;
+
+public class Project1 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("\n = = = = = =  Question 1  = = = = = = = = = \n");
+        SDES.run();
+        
+        System.out.println("\n = = = = = =  Question 2  = = = = = = = = = \n");
+        TripleDES.run();
+        
+        System.out.println("\n = = = = = =  Question 3  = = = = = = = = = \n");
+        CrackingEncryption.run();
+    }
+    
+}
